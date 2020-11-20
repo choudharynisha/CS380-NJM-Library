3579678447f7aaf72f83ea109b917f14504e2f24
Nisha Choudhary <choudharynisha024@gmail.com>
2020-11-19 21:03:06 -0500

    Able to Add Patrons

--- addpatron.php
+++ addpatron.php
@@ -8,36 +8,59 @@
         $email = $_POST['email'];
         $firstname = $_POST['firstname'];
         $lastname = $_POST['lastname'];
-        $street = $_POST['streetaddress'];
+        $street = $_POST['address'];
         $town = $_POST['town'];
         $state = $_POST['state'];
         $zip = $_POST['zip'];
+        $error = "";
 
         // form validation
         if((strlen($username) < 5) or (strlen($username) > 20)) {
-            die("Invalid Username");
+            $error = "Invalid Username";
         }
 
         if((strlen($password) < 6) or (strlen($password) > 20)) {
-            die("Invalid Password");
+            $error = "Invalid Password";
+        } else {
+            $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
         }
         
         if((strlen($state) < 4) or (strlen($state) > 50)) {
-            die("Invalid State Name");
+            $error = "Invalid State Name";
         }
 
         if(strlen($zip) != 5) {
-            die("Invalid Zip Code");
+            $error = "Invalid Zip Code";
         }
 
-        // add new patron
-        $addpatron = "INSERT INTO njm_users (username, password, email, role, first_name, last_name, street_address, town, state, zip) VALUES ($username, $password, $email, 'borrower', $firstname, $lastname, $street, $town, $state, $zip)";
+        $checkusers = "SELECT * FROM njm_users WHERE username = '$username' OR email = '$email'";
+        $results = $connection->query($checkusers);
         
-        if($connnection->query($addpatron) === TRUE) {
-            // successful addition of the new book to njm_books
-            echo "New patron added successfuly";
+        if($results->num_rows > 0) {
+            $error = "Username or email exists";
+        }
+        
+        // add new patron
+        if(strlen($error) == 0) {
+            $addpatron = "INSERT INTO njm_users (username, password, email, role, first_name, last_name, street_address, town, state, zip) VALUES ('$username', '$hashedpassword', '$email', 'borrower', '$firstname', '$lastname', '$street', '$town', '$state', '$zip')";
+            
+            if($connection->query($addpatron) === TRUE) {
+                // successful addition of the new book to njm_books
+                // reset the variables so that the librarian can add a new patron, if wanted
+                $username = "";
+                $password = "";
+                $email = "";
+                $firstname = "";
+                $lastname = "";
+                $street = "";
+                $town = "";
+                $state = "";
+                $zip = "";
+            } else {
+                echo "Error – " . $addpatron . "<br>" . $connection->error;
+            }
         } else {
-            echo "Error – " . $addpatron . "<br>" . $connnection->error;
+            echo $error;
         }
     }
 ?>
@@ -49,31 +72,31 @@
     <body>
         <form action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
             Username
-            <input type = "text" name = "username" required /><br /><br />
+            <input type = "text" name = "username" value = "<?php if(isset($_POST['username'])) {echo $username;} ?>" required /><br /><br />
 
             Password
-            <input type = "text" name = "password" required /><br /><br />
+            <input type = "password" name = "password" value = "<?php if(isset($_POST['password'])) {echo $password;} ?>" required /><br /><br />
 
             Email
-            <input type = "email" name = "email" required /><br /><br />
+            <input type = "email" name = "email" value = "<?php if(isset($_POST['email'])) {echo $email;} ?>" required /><br /><br />
 
             First Name
-            <input type = "text" name = "firstname" required /><br /><br />
+            <input type = "text" name = "firstname" value = "<?php if(isset($_POST['firstname'])) {echo $firstname;} ?>" required /><br /><br />
 
             Last Name
-            <input type = "text" name = "lastname" required /><br /><br />
+            <input type = "text" name = "lastname" value = "<?php if(isset($_POST['lastname'])) {echo $lastname;} ?>" required /><br /><br />
 
             Street Address
-            <input type = "text" name = "address" required /><br /><br />
+            <input type = "text" name = "address" value = "<?php if(isset($_POST['address'])) {echo $street;} ?>" required /><br /><br />
 
             Town
-            <input type = "text" name = "town" required /><br /><br />
+            <input type = "text" name = "town" value = "<?php if(isset($_POST['town'])) {echo $town;} ?>" required /><br /><br />
 
             State
-            <input type = "text" name = "state" required /><br /><br />
+            <input type = "text" name = "state" value = "<?php if(isset($_POST['state'])) {echo $state;} ?>" required /><br /><br />
 
             Zip Code
-            <input type = "text" name = "zip" required /><br /><br />
+            <input type = "text" name = "zip" value = "<?php if(isset($_POST['zip'])) {echo $zip;} ?>" required /><br /><br />
 
             <input type = "submit" value = "submit" name = "submit" />
         </form>

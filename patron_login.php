11722e8a3b5be8a242c8b24c15b55dd61e698438
Nisha Choudhary <choudharynisha024@gmail.com>
2020-12-08 15:40:07 -0500

    Fixed the Strange Password Verifcation Bug

--- patron_login.php
+++ patron_login.php
@@ -4,7 +4,6 @@
     if(isset($_POST['submit'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $hashed = password_hash($password, PASSWORD_DEFAULT);
 
         $getuser = "SELECT password FROM njm_users WHERE username = '$username' AND role = 'borrower'";
 
@@ -22,104 +21,34 @@
         }
     }
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
-    <meta charset = "utf-8">
-    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script defer src = "login-page.js"></script>
+    <head>
+    </head>
+    <body>
+        <form action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
+            Username
+            <input id = "username" type = "text" name = "username" required /><br /><br />
 
-    <style>
-        * {
-            box-sizing: border-box;
-        }
+            Password
+            <input type = "password" name = "password" required /><br /><br />
 
-        body, html {
-            height: 100%;
-            margin: 0;
-            font-family: Arial, Helvetica, sans-serif;
-            background: rgb(245, 243, 243);
-        }    
-    </style>
-</head>
-<body>
-<div class="hero-image">
-
-    <div id="navbar">
-        <a class="active" href="javascript:void(0)">Home</a>
-        <a href="javascript:void(0)">News</a>
-        <a href="javascript:void(0)">Contact</a>
-      
-        <div class="logo">
-          <!--<img src="lanternz.gif">-->
-          <h1 style="color: yellow; font-size: 25px;text-align: center;">NJM Online Library</h1>
-      </div>
-  </div>
-
-
-  </div>
-<div class="row">
-  <div class="leftcolumn">
-    <div class="card2">
-      <h2 style="text-align: center;">Log in with your Patron userid and password</h2>
-      <div class = "box">
-        
-      <h1 style = "text-align: center; font-size: 35px;">Librarian Login</h1>
-            <div id = "login-error-msg-holder">
-                <p id = "login-error-msg">Invalid username<span id="error-msg-second-line">and/or password</span></p>
-            </div>
-      <form id = "login-form" method = "post" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
-                <input type = "text" name = "username" id = "username-field" class = "login-form-field" placeholder = "Username" required>
-                <input type = "password" name = "password" id = "password-field" class = "login-form-field" placeholder = "Password" required>
-                <input type = "submit" name = "submit" value = "Login" id = "login-form-submit">
+            <input type = "submit" value = "submit" name = "submit" />
         </form>
-    </div>
-    </div>
-  </div>
-  <div class="rightcolumn">
-    <div class="card">
-        <h4><a href="#">Log in</a></h4>
-        <h4><a href="#">Request Librarian Help</a></h4>
-        <h4><a href="#">Feedback</a></h4>
-    </div>
-    <div class="card">
-      <h3>Monthly Book Club Reads</h3>
-      <div class="fakeimg">
-        <img src="persuasion_ja.jpg"> 
-      </div>
-      <div class="fakeimg"><img src="anxious_people.jpeg"> </div>
-    </div>
-  </div>
-</div>
 
-<div class="footer">
-    <p style="color:white;  text-align: center; ">
-        <br><br>
-        Contact us @
-        Email: ouremail.brynmawr.edu <br>
-        Mobile: +1 610 526 5000
-    </p>
-</div>
+        <a href = "javascript:reset_password()">Forgot Password</a>
 
-<script>
+        <script>
+            function reset_password() {
+                var username = document.getElementById("username").value.trim();
 
-
-    window.onscroll = function() {myFunction()};
-    
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-    
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
-    </script>
-
-</body>
+                if(username.length > 0) {
+                    window.location = `reset_password.php?username=${username}`;
+                } else {
+                    alert("Please enter your username before requesting to reset password.");
+                    return false;
+                }
+            }
+        </script>
+    </body>
 </html>

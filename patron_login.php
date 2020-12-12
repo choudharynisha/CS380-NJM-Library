4ca4ab686653715c6c0f134d3141e6837eabd991
Nisha Choudhary <choudharynisha024@gmail.com>
2020-12-11 19:11:26 -0500

    Calls the JavaScript resetPassword() Function instead of Going Directly to reset_password.php

--- patron_login.php
+++ patron_login.php
@@ -49,82 +49,76 @@
         }    
     </style>
 </head>
-<body data-new-gr-c-s-check-loaded="14.984.0" data-gr-ext-installed="">
-<div class="hero-image">
-
-    <div id="navbar">
-         <a href="index.php"> Home</a>
-        <a href="navBookTable.php"> Books</a>
-        <a href="#"> Contact Us</a>
-      
-        <div class="logo">
-          <!--<img src="lanternz.gif">-->
-          <h1 style="color: yellow; font-size: 25px;text-align: center;">NJM Online Library</h1>
-        </div>
-  </div>
-</div>
-<div class="row">
-  <div class="leftcolumn">
-    <div class="card2">
-      <div class="fakeimg">
-      <div class = "box">
-        
-      <h1 style = "text-align: center; font-size: 35px;">Patron Login</h1>
-            <div id = "login-error-msg-holder">
-                <p id = "login-error-msg">Invalid username<span id="error-msg-second-line">and/or password</span></p>
+    <body data-new-gr-c-s-check-loaded="14.984.0" data-gr-ext-installed="">
+    <div class = "hero-image">
+        <div id = "navbar">
+            <a href = "index.php">Home</a>
+            <a href = "navBookTable.php">Books</a>
+            <a href="#"> Contact Us</a>
+            
+            <div class = "logo">
+                <!--<img src="lanternz.gif">-->
+                <h1 style = "color: yellow; font-size: 25px;text-align: center;">NJM Online Library</h1>
             </div>
-
-            <form action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
-            Username
-            <input id = "username" type = "text" name = "username" required /><br /><br />
-            Password
-            <input type = "password" name = "password" required /><br /><br />
-            <input  type = "submit" value = "submit" name = "submit" />
-        </form>
-        <a href="reset_password.php" class="button-basic" style= "margin:auto; padding-bottom: 15px;">Forgot Password</a>
-
+        </div>
     </div>
-    </div>
-    </div>
-  </div>
-  <div class="rightcolumn">
-    <div class="card">
-      <h3>Monthly Book Club Reads</h3>
-      <div class="fakeimg">
-        <img src="images/persuasion_ja.jpg"> 
-      </div>
-      <div class="fakeimg"><img src="images/anxious_people.jpeg"> </div>
-    </div>
-  </div>
-</div>
+    <div class = "row">
+            <div class = "leftcolumn">
+            <div class = "card2">
+                <div class = "fakeimg">
+                <div class = "box">
+                
+                <h1 style = "text-align: center; font-size: 35px;">Patron Login</h1>
+                    <div id = "login-error-msg-holder">
+                        <p id = "login-error-msg">Invalid username<span id="error-msg-second-line">and/or password</span></p>
+                    </div>
 
-<div class="footer">
-    <p style="color:white;  text-align: center; ">
-        <br><br>
-        Contact us @
-        Email: ouremail@brynmawr.edu <br>
-        Mobile: +1 610 526 5000
-    </p>
-</div>
+                    <form action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
+                    Username
+                    <input id = "username" type = "text" name = "username" required /><br /><br />
+                    Password
+                    <input type = "password" name = "password" required /><br /><br />
+                    <input  type = "submit" value = "submit" name = "submit" />
+                </form>
+                <a href = "javascript:reset_password()" class = "button-basic" style = "margin:auto; padding-bottom: 15px;">Forgot Password</a>
 
-<script>
+            </div>
+            </div>
+            </div>
+            </div>
+            <div class = "rightcolumn">
+                <div class = "card">
+                    <h3>Monthly Book Club Reads</h3>
+                <div class = "fakeimg">
+                    <img src = "images/persuasion_ja.jpg"> 
+                    </div>
+                    <div class = "fakeimg"><img src = "images/anxious_people.jpeg"></div>
+                </div>
+            </div>
+        </div>
 
+        <div class="footer">
+            <p style="color:white;  text-align: center; ">
+                <br><br>
+                Contact us @
+                Email: ouremail@brynmawr.edu <br>
+                Mobile: +1 610 526 5000
+            </p>
+        </div>
+        <script>
+            window.onscroll = function() {myFunction()};
+        
+            var navbar = document.getElementById("navbar");
+            var sticky = navbar.offsetTop;
+        
+            function myFunction() {
+                if (window.pageYOffset >= sticky) {
+                navbar.classList.add("sticky")
+                } else {
+                navbar.classList.remove("sticky");
+                }
+            }
 
-    window.onscroll = function() {myFunction()};
-    
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-    
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
-    </script>
-
-    <script>
             function reset_password() {
                 var username = document.getElementById("username").value.trim();
 
@@ -136,6 +130,5 @@
                 }
             }
         </script>
-
-</body>
+    </body>
 </html>

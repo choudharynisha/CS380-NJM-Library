200aade856807e021f10234c3846c60389cc14da
Nisha Choudhary <choudharynisha024@gmail.com>
2020-12-12 22:06:03 -0500

    Fixed Links and Some Styling

--- libBookTable.php
+++ libBookTable.php
@@ -2,93 +2,87 @@
     include 'db.php';
     session_start();
     $expiry = 5400; //90 min is 5400 sec
-        if (isset($_SESSION['login_time']) && (time() - $_SESSION['login_time'] > $expiry)) {
-            session_unset();
-            session_destroy();
-            echo "<script>
-                alert('Please login again. Your session expired'); 
-                window.location.href = 'main_login.php';
-                </script>";
-        }
+    if(isset($_SESSION['login_time']) && (time() - $_SESSION['login_time'] > $expiry)) {
+        session_unset();
+        session_destroy();
+        echo "<script>
+            alert('Please login again. Your session expired'); 
+            window.location.href = 'main_login.php';
+            </script>";
+    }
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" type="text/css" href="bookTableStyle.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <meta name = "viewport" content = "width=device-width, initial-scale=1">
+        <link rel = "stylesheet" href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/style.css">
+        <link rel = "stylesheet" type = "text/css" href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/bookTableStyle.css">
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script>
             //filterFunction deals with filtering the columns of the table
             function filterFunction() {
 
-            //get the input values to filter
-            var bookInput, authorInput, genreInput, yearInput, bookFilter,  authorFilter, genreFilter, yearFilter, table, tr, td, i, txtValue;
-            bookInput = document.getElementById("bookInput"); 
-            authorInput = document.getElementById("authorInput"); 
-            genreInput = document.getElementById("genreInput");
-            yearInput = document.getElementById("yearInput");
-            publisherInput = document.getElementById("publisherInput");
+                //get the input values to filter
+                var bookInput, authorInput, genreInput, yearInput, bookFilter,  authorFilter, genreFilter, yearFilter, table, tr, td, i, txtValue;
+                bookInput = document.getElementById("bookInput"); 
+                authorInput = document.getElementById("authorInput"); 
+                genreInput = document.getElementById("genreInput");
+                yearInput = document.getElementById("yearInput");
+                publisherInput = document.getElementById("publisherInput");
 
-            bookFilter = bookInput.value.toUpperCase(); 
-            authorFilter = authorInput.value.toUpperCase(); 
-            genreFilter = genreInput.value.toUpperCase();
-            yearFilter = yearInput.value.toUpperCase();
-            publisherFilter = publisherInput.value.toUpperCase();
+                bookFilter = bookInput.value.toUpperCase(); 
+                authorFilter = authorInput.value.toUpperCase(); 
+                genreFilter = genreInput.value.toUpperCase();
+                yearFilter = yearInput.value.toUpperCase();
+                publisherFilter = publisherInput.value.toUpperCase();
 
-            //get the table to search through 
-            table = document.getElementById("result");
-            tr = table.getElementsByTagName("tr");
+                //get the table to search through 
+                table = document.getElementById("result");
+                tr = table.getElementsByTagName("tr");
 
-            //go through values of the table and filter by inputs and skip the header and the inputs rows
-            for (i = 2; i < tr.length; i++) {
-                //checking the columns based on inputs
-                bookTd = tr[i].getElementsByTagName("td")[0];
-                authorTd = tr[i].getElementsByTagName("td")[1];
-                genreTd = tr[i].getElementsByTagName("td")[2];
-                yearTd = tr[i].getElementsByTagName("td")[3];
-                publisherTd = tr[i].getElementsByTagName("td")[4];
-                if (bookTd && authorTd && genreTd && yearTd && publisherTd) {
-                    txtValueBook = bookTd.textContent || bookTd.innerText;
-                    txtValueAuthor = authorTd.textContent || authorTd.innerText;
-                    txtValueGenre = genreTd.textContent || genreTd.innerText;
-                    txtValueYear = yearTd.textContent || yearTd.innerText;
-                    txtValuePublisher = publisherTd.textContent || publisherTd.innerText;
-                    if (txtValueBook.toUpperCase().indexOf(bookFilter) > -1 &&
-                        txtValueAuthor.toUpperCase().indexOf(authorFilter) > -1 &&
-                        txtValueGenre.toUpperCase().indexOf(genreFilter) > -1 &&
-                        txtValueYear.toUpperCase().indexOf(yearFilter) > -1 &&
-                        txtValuePublisher.toUpperCase().indexOf(publisherFilter) > -1) {
-                        tr[i].style.display = "";
-                    } else {
-                        tr[i].style.display = "none";
+                //go through values of the table and filter by inputs and skip the header and the inputs rows
+                for (i = 2; i < tr.length; i++) {
+                    //checking the columns based on inputs
+                    bookTd = tr[i].getElementsByTagName("td")[0];
+                    authorTd = tr[i].getElementsByTagName("td")[1];
+                    genreTd = tr[i].getElementsByTagName("td")[2];
+                    yearTd = tr[i].getElementsByTagName("td")[3];
+                    publisherTd = tr[i].getElementsByTagName("td")[4];
+                    if(bookTd && authorTd && genreTd && yearTd && publisherTd) {
+                        txtValueBook = bookTd.textContent || bookTd.innerText;
+                        txtValueAuthor = authorTd.textContent || authorTd.innerText;
+                        txtValueGenre = genreTd.textContent || genreTd.innerText;
+                        txtValueYear = yearTd.textContent || yearTd.innerText;
+                        txtValuePublisher = publisherTd.textContent || publisherTd.innerText;
+                        if(txtValueBook.toUpperCase().indexOf(bookFilter) > -1 &&
+                            txtValueAuthor.toUpperCase().indexOf(authorFilter) > -1 &&
+                            txtValueGenre.toUpperCase().indexOf(genreFilter) > -1 &&
+                            txtValueYear.toUpperCase().indexOf(yearFilter) > -1 &&
+                            txtValuePublisher.toUpperCase().indexOf(publisherFilter) > -1) {
+                            tr[i].style.display = "";
+                        } else {
+                            tr[i].style.display = "none";
+                        }
                     }
                 }
-            }
 
             }
 
             $(document).ready(function() {
-
-            //Toggles the Show or Hide Borrowed div
-            $('.viewBooks').click(function(event) {
-                event.stopPropagation();
-                $(".showup").slideToggle("fast");
-                $('.showup').load('loadBorrowBooksDiv.php', function() {  //loads the php file to update the div with new borrowed books
-                    console.log('Load was performed.');
+                //Toggles the Show or Hide Borrowed div
+                $('.viewBooks').click(function(event) {
+                    event.stopPropagation();
+                    $(".showup").slideToggle("fast");
+                    $('.showup').load('loadBorrowBooksDiv.php', function() {  //loads the php file to update the div with new borrowed books
+                        console.log('Load was performed.');
+                    });
                 });
 
-            });
-
-            //Toggles the the text for Show or Hide Borrowed button
-            $('.viewBtn').click(function(event) {
-                var button = $(this);
-                button.text(button.text() == "Hide Borrowed" ? "Show Borrowed" : "Hide Borrowed")
-            });
-
+                //Toggles the the text for Show or Hide Borrowed button
+                $('.viewBtn').click(function(event) {
+                    var button = $(this);
+                    button.text(button.text() == "Hide Borrowed" ? "Show Borrowed" : "Hide Borrowed")
+                });
             });
         </script>
 
@@ -106,21 +100,21 @@
         </style>
     </head>
 <body>
-    <div class="hero-image">
-        <div id="navbar">
+    <div class = "hero-image">
+        <div id = "navbar">
 
-        <a href="librarian_index.php"> Home</a>
-            <a href="lib_nav.php"> Books</a>
-            <a href="logout.php"> Log Out</a>
-            <div class="logo"><h1 style="color: yellow; font-size: 25px;text-align: center;">NJM Online Library</h1></div>
+        <a href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/librarian_index.php"> Home</a>
+            <a href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/lib_nav.php"> Books</a>
+            <a href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/logout.php"> Log Out</a>
+            <div class = "logo"><h1 style = "color: yellow; font-size: 25px;text-align: center;">NJM Online Library</h1></div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="leftcolumn">
-            <div class="card2">
+    <div class = "row">
+        <div class = "leftcolumn">
+            <div class = "card2">
                     <h1>Available Books. Click Borrow button to lend</h1>
-                    <div class='listing' id='listing'>
+                    <div class = 'listing' id = 'listing'>
                         <?php
                             ob_start();
                         
@@ -141,7 +135,7 @@
                                     where njm_books.status = 'Available'";
                         
                             $result = $connection->query($q);
-                            if ($result->num_rows > 0) {
+                            if($result->num_rows > 0) {
                                 echo "
                                 
                                 <table id='result'>
@@ -163,7 +157,7 @@
                                 </tr>
                                 ";
                                 echo "<tbody class='bookRows' id = 'bookRows'>";
-                                while ($row = $result->fetch_assoc()) {
+                                while($row = $result->fetch_assoc()) {
                                     $title = $row['title'];
                                     $author = $row['author'];
                                     $book_id = $row['book_id'];
@@ -184,15 +178,13 @@
                                 
                                 echo "</tbody>";
                                 
-
                                 //checks if form has been submitted and adds to the transaction table and changes the status in the books table
-                                if (isset($_POST['title']) && isset($_POST['author'])){
-
+                                if(isset($_POST['title']) && isset($_POST['author'])){
                                     $dueDate = date('Y-m-d',strtotime('+7 day')); //gets the current date and adds a week to it
                                     $q = "insert into njm_transactions (transaction_type, book_id, user_id, due_date) values 
                                     ('borrowed', '".$_POST['book_id']."', '$id', '$dueDate');";  //need to change 14 to user_id
 
-                                    if (mysqli_query($connection, $q)) {
+                                    if(mysqli_query($connection, $q)) {
                                         //shows alert box to indicate that a book has been borrowed
                                         echo "<script>
                                                 alert('".$_POST['title']." By ".$_POST['author']." Added Successfully'); 
@@ -210,70 +202,57 @@
                                     } else {
                                         echo "Error: " . $statusChange . "<br>" . mysqli_error($connection);
                                     }
-
-                                    //header("Location: ".$_SERVER['REQUEST_URI']); //reloads the page to update the changes in the table but no longer nee since alert box does refreshing
-
-                                
                                 }
-
-                            }
-                            else {
+                            } else {
                                 echo "Did not Work!";
                             }
+
                             echo "</table>";
-                            
                         ?>
                     </div>
                     <br>
-                    <div class="viewBooks"><button class="viewBtn">Show Borrowed</button></div>
-                    <div class="showup" id="showup"></div>
-
+                <div class = "viewBooks"><button class = "viewBtn">Show Borrowed</button></div>
+                <div class = "showup" id = "showup"></div>
             </div>
         </div>
 
-        <div class="rightcolumn">
-            <div class="card">
-                <h4><a href="main_login.php">Log Out</a></h4>
-                <h4><a href="lib_add_patron.php">Add New Patron Account</a></h4>
-                <h4><a href="addbook.php">Add New Book Record</a></h4>
-                <h4><a href="librarianViewBorrowed.php">Return Borrowed Books</a></h4>
+        <div class = "rightcolumn">
+            <div class = "card">
+                <h4><a href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/main_login.php">Log Out</a></h4>
+                <h4><a href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/lib_add_patron.php">Add New Patron Account</a></h4>
+                <h4><a href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/addbook.php">Add New Book Record</a></h4>
+                <h4><a href = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/librarianViewBorrowed.php">Return Borrowed Books</a></h4>
             </div>
-            <div class="card">
+            <div class = "card">
                 <h3>Monthly Book Club Reads</h3>
-                <div class="fakeimg"><img src="images/persuasion_ja.jpg"></div><br>
-                <div class="fakeimg"><img src="images/anxious_people.jpeg"> </div><br>
+                <div class = "fakeimg"><img src = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/images/persuasion_ja.jpg"></div><br>
+                <div class = "fakeimg"><img src = "http://comet.cs.brynmawr.edu/~nchoudhary/CS380-Library-System/images/anxious_people.jpeg"> </div><br>
             </div>
     
         </div>
     </div>
- 
+    <div class = "footer">
+        <p style = "color:white;  text-align: center; ">
+            <br><br>
+            Contact us @
+            Email: ouremail@brynmawr.edu <br>
+            Mobile: +1 610 526 5000
+        </p>
+    </div>
 
-
-<div class="footer">
-    <p style="color:white;  text-align: center; ">
-        <br><br>
-        Contact us @
-        Email: ouremail@brynmawr.edu <br>
-        Mobile: +1 610 526 5000
-    </p>
-</div>
-
-<script>
-
-
-    window.onscroll = function() {myFunction()};
-    
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-    
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
+    <script>
+        window.onscroll = function() {myFunction()};
+        
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+        
+        function myFunction() {
+            if (window.pageYOffset >= sticky) {
+                navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+            }
+        }
     </script>
-
 </body>
 </html>
